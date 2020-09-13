9062862f7146ce2b8070afc1b4eaa355ae0dd075
Yongwoo Huh <yongwoo.huh@me.com>
2020-09-14 07:07:06 +0900

    Finish day 001.

---
+++ day001.playground/Contents.swift
@@ -0,0 +1,50 @@
+import UIKit
+// # Variables
+//var str = "Hello, playground"
+//str = "Goodbye"
+
+// # Strings and integers
+//var age = 38
+//var population = 8_000_000
+// not posible
+//str = 38
+//var meaningOfLife = 42
+//meaningOfLife = "Forty two"
+
+// # Multi-line strings
+//var str1 = """
+//This goes
+//over multiple
+//lines
+//"""
+//
+//var str2 = """
+//This goes \
+//over multiple \
+//lines
+//"""
+
+// # Doubles and booleans
+//var pi = 3.141
+//var awesome = true
+//var myInt = 1
+//var myDouble = 1.0
+//var total = myInt + myDouble
+
+// # String interpolation
+//var score = 85
+//var str = "Your score \(score)"
+//var resutls = "The test results are here: \(str)"
+//var city = "Cardiff"
+//var message = "Welcome to \(city)"
+
+// # Constants
+//let taylor = "Swift"
+
+// # Type annotations
+//let str = "Hello, playground"
+//let album: String = "Reputation"
+//let year: Int = 1989
+//let height: Double = 1.78
+//let taylorRocks: Bool = true
+//var percentage: Double = 99
