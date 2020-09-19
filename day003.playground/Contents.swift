3c37256d6bc5fc5954c92e4a66a86dee28052668
Yongwoo Huh <yongwoo.huh@me.com>
2020-09-19 09:31:10 +0900

    Day 3
    Subjects learned
    - Arithmetic operators
    - Operator overloading
    - Compound assignment operators
    - Comparison operators
    - Conditions
    - Combining conditions
    - The ternary operator
    - Switch statements
    - Range operators

---
+++ day003.playground/Contents.swift
@@ -0,0 +1,149 @@
+import UIKit
+
+// # Arithmetic operators
+//let firstScore = 12
+//let secondScore = 4
+//let total = firstScore + secondScore
+//let difference = firstScore - secondScore
+//let product = firstScore * secondScore
+//let divided = firstScore / secondScore
+//let remainder = 13 % secondScore
+//let value: Int = 90000000000000001
+//
+//let weeks = 465 / 7
+//print("There are \(weeks) weeks until the event.")
+//
+//let weeksInDouble: Double = 465 / 7
+//print("There are \(weeksInDouble) weeks until the event.")
+//
+//let weeks2 = 465 / 7
+//let days = 465 % 7
+//print("There are \(weeks2) weeks and \(days) days until the event.")
+//
+//let number = 465
+//let isMultiple = number.isMultiple(of: 7)
+
+// # Operator overloading
+//let meaningOfLife = 42
+//let doubleMeaning = 42 + 42
+//
+//let fakers = "Fakers gonna "
+//let action = fakers + "fake"
+//
+//let firstHalf = ["John", "Paul"]
+//let secondHalf = ["George", "Ringo"]
+//let beatles = firstHalf + secondHalf
+
+// # Compound assignment operators
+//var score = 95
+//score -= 5
+//
+//var quote = "The rain in Spain falls mainly on the "
+//quote += "Spaniards"
+
+// # Comparison operators
+//let firstScore = 6
+//let secondScore = 4
+//
+//firstScore == secondScore
+//firstScore != secondScore
+//firstScore < secondScore
+//firstScore >= secondScore
+//
+//"Taylor" <= "Swift"
+//
+//let firstName = "Paul"
+//let secondName = "Sophie"
+//
+//let firstAge = 40
+//let secondAge = 10
+//
+//print(firstName == secondName)
+//print(firstName != secondName)
+//print(firstName < secondName)
+//print(firstName >= secondName)
+//
+//print(firstAge == secondAge)
+//print(firstAge != secondAge)
+//print(firstAge < secondAge)
+//print(firstAge >= secondAge)
+//
+//enum Sizes: Comparable {
+//    case small
+//    case medium
+//    case large
+//}
+//
+//let first = Sizes.small
+//let second = Sizes.large
+//print(first < second)
+
+// # Conditions
+//let firstCard = 11
+//let secondCard = 10
+//
+//if firstCard + secondCard == 21 {
+//    print("Blackjack!")
+//} else {
+//    print("Regular cards")
+//}
+//
+//if firstCard + secondCard == 2 {
+//    print("Aces - lucky!")
+//} else if firstCard + secondCard == 21 {
+//    print("Blackjack!")
+//} else {
+//    print("Regular cards")
+//}
+
+// # Combining conditions
+//let age1 = 12
+//let age2 = 21
+//
+//if age1 > 18 && age2 > 18 {
+//    print("Both are over 18")
+//}
+//
+//if age1 > 18 || age2 > 18 {
+//    print("At least one is over 18")
+//}
+
+// # The ternary operator
+//let firstCard = 11
+//let secondCard = 10
+//print(firstCard == secondCard ? "Cards are the same" : "Cards are different")
+//
+//let isAuthenticated = false
+//print(isAuthenticated ? "Welcome!" : "Who are you?")
+
+// # Switch statments
+//let weather = "sunny"
+//switch weather {
+//case "rain":
+//    print("Bring an umbrella")
+//case "snow":
+//    print("Wrap up warm")
+//case "sunny":
+//    print("Wear sunscreen")
+//    fallthrough
+//default:
+//    print("Enjoy your day!")
+//}
+
+// # Range operators
+//let score = 86
+//
+//switch score {
+//case 0..<50:
+//    print("You failed badly.")
+//case 50..<85:
+//    print("You did OK.")
+//default:
+//    print("You did great!")
+//}
+//
+//let names = ["Piper", "Alex", "Suzanne", "Gloria"]
+//print(names[0])
+//print(names[1...3])
+//print(names[1...])
+
